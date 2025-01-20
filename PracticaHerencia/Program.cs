bfb52dae3dbc056ad2f913f5afa4d089edef7864
ana galvez <130255411+Ana-Galvez@users.noreply.github.com>
2025-01-19 22:35:43 -0300

    feat: class mamiferos

--- PracticaHerencia/Program.cs
+++ PracticaHerencia/Program.cs
@@ -9,4 +9,32 @@ namespace PracticaHerencia
 
         }
     }
+
+    class Mamiferos
+    {
+        public void Respirar()
+        {
+            Console.WriteLine("Soy capaz de respirar");
+        }
+
+        public void CuidarCrias()
+        {
+            Console.WriteLine("Cuido de mis crías hasta que puedan valerse por sí mismas");
+        }
+    }
+
+    class Caballo
+    {
+
+    }
+
+    class Humano
+    {
+
+    }
+
+    class Gorila
+    {
+
+    }
 }
