6109cf9688ffbb62e166ced4e758b80797fe95e8
ana galvez <130255411+Ana-Galvez@users.noreply.github.com>
2025-01-23 23:18:17 -0300

    fix: base()

--- PracticaHerencia/notas.cs
+++ PracticaHerencia/notas.cs
@@ -1,4 +1,4 @@
 ﻿/*
  CONSTRUCTORES: de los hijos llaman al contructor padre por medio de :base()  "lo hace implícitamente c#"
-PERO  si creamos un constructor este reemplaza al constructor por defecto
+PERO  si creamos un constructor este reemplaza al constructor por defecto y debemos usar :base()
  */
