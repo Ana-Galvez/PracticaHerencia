7f3d4be2c1c6e3a0397248aa5759aee2c39da9c4
ana galvez <130255411+Ana-Galvez@users.noreply.github.com>
2025-01-25 23:49:49 -0300

    feat: principio de sustitucion

--- PracticaHerencia/Program.cs
+++ PracticaHerencia/Program.cs
@@ -7,66 +7,67 @@ namespace PracticaHerencia
     {
         static void Main(string[] args)
         {
-            Caballo caballo1 = new Caballo("Bucéfalo");
-            Caballo caballo2 = new Caballo("Génitor");
-            Humano humano1 = new Humano("Ana");
-            Gorila gorila1 = new Gorila("Koko");
-            caballo2.GetNombre();
+            //Caballo caballo1 = new Caballo("Bucéfalo");
+            //Caballo caballo2 = new Caballo("Génitor");
+            //Humano humano1 = new Humano("Ana");
+            //Gorila gorila1 = new Gorila("Koko");
+            //caballo2.GetNombre();
+            //Mamifero humano2 = new Humano("Tatin");
            
         }
     }
 
-    class Mamiferos
-    {
-        private String nombreSerVivo;
+    //class Mamiferos
+    //{
+    //    private String nombreSerVivo;
 
-        public Mamiferos(string nombreSerVivo)
-        {
-            this.nombreSerVivo = nombreSerVivo;
-        }
+    //    public Mamiferos(string nombreSerVivo)
+    //    {
+    //        this.nombreSerVivo = nombreSerVivo;
+    //    }
 
-        public void Respirar()
-        {
-            Console.WriteLine("Soy capaz de respirar");
-        }
+    //    public void Respirar()
+    //    {
+    //        Console.WriteLine("Soy capaz de respirar");
+    //    }
 
-        public void CuidarCrias()
-        {
-            Console.WriteLine("Cuido de mis crías hasta que puedan valerse por sí mismas");
-        }
+    //    public void CuidarCrias()
+    //    {
+    //        Console.WriteLine("Cuido de mis crías hasta que puedan valerse por sí mismas");
+    //    }
 
-        public void GetNombre()
-        {
-            Console.WriteLine("Mi nombre es " + nombreSerVivo);
-        }
+    //    public void GetNombre()
+    //    {
+    //        Console.WriteLine("Mi nombre es " + nombreSerVivo);
+    //    }
 
 
-    }
+    //}
 
-    class Caballo: Mamiferos 
-    {
-        public Caballo(String nombreCaballo) : base(nombreCaballo) { }
-        public void Galopar()
-        {
-            Console.WriteLine("Puedo galopar");
-        }
-    }
+    //class Caballo: Mamiferos 
+    //{
+    //    public Caballo(String nombreCaballo) : base(nombreCaballo) { }
+    //    public void Galopar()
+    //    {
+    //        Console.WriteLine("Puedo galopar");
+    //    }
+    //}
 
-    class Humano : Mamiferos 
-    {
-        public Humano(String nombreHumano) : base(nombreHumano) { }
-        public void Pensar()
-        {
-            Console.WriteLine("Puedo pensar");
-        }
-    }
+    //class Humano : Mamiferos 
+    //{
+    //    public Humano(String nombreHumano) : base(nombreHumano) { }
+    //    public void Pensar()
+    //    {
+    //        Console.WriteLine("Puedo pensar");
+    //    }
+    //}
 
-    class Gorila : Mamiferos
-    {
-        public Gorila(String nombreGorila):base(nombreGorila) { }
-        public void Trepar()
-        {
-            Console.WriteLine("Puedo trepar");
-        }
-    }
+    //class Gorila : Mamiferos
+    //{
+    //    public Gorila(String nombreGorila):base(nombreGorila) { }
+    //    public void Trepar()
+    //    {
+    //        Console.WriteLine("Puedo trepar");
+    //    }
+    //}
 }
--- PracticaHerencia/notas.cs
+++ PracticaHerencia/notas.cs
@@ -2,4 +2,6 @@
  CONSTRUCTORES: de los hijos llaman al contructor padre por medio de :base()  "lo hace implícitamente c#"
 PERO  si creamos un constructor este reemplaza al constructor por defecto y debemos usar :base() en constructor hijo
 public Caballo (parametro para igualar parametro constructor padre):base(parametro constructor padre){}
+PRINCIPIO DE SUSTITUCIÓN: "....SIEMPRE ES UN.." ej  humano es siempre un mamífero"  entonces humano hereda de mamifero
+
  */
