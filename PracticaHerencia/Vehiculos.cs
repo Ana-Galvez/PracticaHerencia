c35ce6e5be3eb5360a65c967fe73c65ea786c997
ana galvez <130255411+Ana-Galvez@users.noreply.github.com>
2025-02-01 00:57:09 -0300

    feat: ejercicio herencia

---
+++ PracticaHerencia/Vehiculos.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class  Vehiculo
+{
+    public void ArrancarMotor()
+    {
+        Console.WriteLine("Motor arrancado");
+    }
+
+    public void PararMotor()
+    {
+        Console.WriteLine("Parar motor");
+    }
+
+    public virtual void Conducir()
+    {
+        Console.WriteLine("Este vehículo se conduce");
+    }
+}
+
+class Avion : Vehiculo
+{
+    public override void Conducir()
+    {
+        Console.WriteLine("Este avión se puede conducir");
+    }
+}
+
+class Coche : Vehiculo
+{
+    public override void Conducir()
+    {
+        Console.WriteLine("Este coche se puede conducir");
+    }
+}
